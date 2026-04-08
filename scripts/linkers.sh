149698100bd490afe606cf7e8c0cc8c520859f13
Odin Bjørnebo <odinbjoernebo@gmail.com>
2026-04-08 20:45:33 +0200

    linking: link .config/* dirs

--- scripts/linkers.sh
+++ scripts/linkers.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env sh
 
-ln -sf "$PWD/🏡/.gitconfig" ~/.gitconfig
-ln -sf "$PWD/🏡/.gitconfig_github" ~/.gitconfig_github
-ln -sf "$PWD/🏡/.gitconfig_gitlab" ~/.gitconfig_gitlab
+REPO_ROOT=$(pwd)
+
+ln -sf "$REPO_ROOT/🏡/.gitconfig" ~/.gitconfig
+ln -sf "$REPO_ROOT/🏡/.gitconfig_github" ~/.gitconfig_github
+ln -sf "$REPO_ROOT/🏡/.gitconfig_gitlab" ~/.gitconfig_gitlab
 
 ln -sf "$PWD/🏡/.zshrc" ~/.zshrc
+
+mkdir -p "$HOME/.config"
+DIRS="$REPO_ROOT/🏡/.config"
+
+for dir in "$DIRS/"*; do
+  [ -e "$dir" ] || continue
+
+  name=$(basename "$dir")
+  target="$HOME/.config/$name"
+
+  rm -rf "$target"
+
+  # -s: symbolic link
+  # -f: overwrite other syms
+  # -n: prevents nesting
+  ln -sfn "$dir" "$target"
+
+  echo "symlinked: $name"
+done

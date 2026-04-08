93c9e733ef005e4bc0ee540d7d186f906a2e8a10
Odin Bjørnebo <odinbjoernebo@gmail.com>
2026-04-08 21:12:23 +0200

    go deps: added staticcheck

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
-set -euo
+set -eu
 
 DEPS="
-mvdan.cc/sh/v3/cmd/shfmt@latest  
-github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.11.4
 golang.org/x/tools/gopls@latest
+mvdan.cc/sh/v3/cmd/shfmt@latest 
+honnef.co/go/tools/cmd/staticcheck@latest
+github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.11.4
 "
 
 for pkg in $DEPS; do

bc0dd705cc7a7fbe6040f6f69172516e2bb6645f
Odin Bjørnebo <odinbjoernebo@gmail.com>
2026-04-09 13:41:13 +0200

    go deps: added goimports and delve

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -6,6 +6,8 @@ golang.org/x/tools/gopls@latest
 mvdan.cc/sh/v3/cmd/shfmt@latest 
 honnef.co/go/tools/cmd/staticcheck@latest
 github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.11.4
+golang.org/x/tools/cmd/goimports@latest
+github.com/go-delve/delve/cmd/dlv@latest
 "
 
 for pkg in $DEPS; do

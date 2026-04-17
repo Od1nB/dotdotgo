d473d606b821ba5de44c7750b5328987af38e9b3
Odin Bjørnebo <odinbjoernebo@gmail.com>
2026-04-17 09:31:23 +0200

    brew deps
    added:
    - llvm
    - gcc
    - lld
    - gh
    - zig
    - docker
    - btop
    - sqlite

--- scripts/brew_install.sh
+++ scripts/brew_install.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env sh
 
 brew install \
+	llvm \
+	gcc \
+	lld \
 	go \
 	fzf \
 	opa \
 	regal \
 	rust \
 	git \
+	gh \
+	zig \
 	ghostty \
 	tree \
+	docker \
+	markdown-oxide \
+	gh \
+	btop \
 	zsh-autosuggestions \
 	shellcheck \
 	awscli \
 	bash-language-server \
+	sqlite \
 	postgresql@18 \
 	k9s \
 	kubectl

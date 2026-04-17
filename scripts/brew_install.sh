8309d5a7eb8caa55ece60150dda2720af035e558
Odin Bjørnebo <odinbjoernebo@gmail.com>
2026-04-17 14:47:56 +0200

    brew deps: added terraform, lsp, and zls

--- scripts/brew_install.sh
+++ scripts/brew_install.sh
@@ -12,6 +12,7 @@ brew install \
 	git \
 	gh \
 	zig \
+	zls \
 	ghostty \
 	tree \
 	docker \
@@ -21,8 +22,12 @@ brew install \
 	zsh-autosuggestions \
 	shellcheck \
 	awscli \
+	terraform-ls \
 	bash-language-server \
 	sqlite \
 	postgresql@18 \
 	k9s \
 	kubectl
+
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
